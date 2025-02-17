c1a7cadadad8561fd1c12ea4fb0137b293f3e4a0
ariana <874055@unizar.es>
2025-02-17 22:38:31 +0100

    Modificado el listado de libros para que aparezcan en 2 columnas

--- src/componentes/ListadoLibros.js
+++ src/componentes/ListadoLibros.js
@@ -1,5 +1,5 @@
 // ListadoLibros.js
-import { StyleSheet, Text, View, Image, ScrollView, TouchableOpacity, FlatList } from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableOpacity, FlatList } from 'react-native';
 import { useEffect, useState } from "react";
 import { useNavigation } from '@react-navigation/native';
 
@@ -25,21 +25,26 @@ export default function ListadoLibros() {
     }
   };
 
+  const renderItem = ({ item }) => (
+    <View style={styles.itemContainer}>
+      <TouchableOpacity onPress={() => navigation.navigate("Detalles", { libro: item })}>
+        <Image 
+          source={{ uri: item.imagen_portada }}
+          style={styles.imagen_portada_libro} 
+        />
+        <Text style={styles.bookTitle}>{item.nombre}</Text>
+      </TouchableOpacity>
+    </View>
+  );
 
   return (
-    <View style={styles.container}>
-      {libros.map(libro => (
-        <View key={libro.enlace}> 
-          <TouchableOpacity onPress={() => navigation.navigate("Detalles", { libro })}>
-            <Image 
-              source={{ uri: libro.imagen_portada }}
-              style={styles.imagen_portada_libro} 
-            />
-            <Text>{libro.nombre}</Text>
-          </TouchableOpacity>
-        </View>
-      ))}
-    </View>
+    <FlatList 
+      data={libros}
+      renderItem={renderItem}
+      keyExtractor={(item) => item.enlace}
+      numColumns={2}  // Mantenemos 2 columnas
+      contentContainerStyle={styles.container}
+    />
   );
 }
 
@@ -47,11 +52,24 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+    paddingHorizontal: 10,
+    paddingTop: 10,
+  },
+  itemContainer: {
+    flex: 1,
+    margin: 5,
+    alignItems: 'center', // Alinea cada item al centro de su contenedor
+    justifyContent: 'center',
   },
   imagen_portada_libro: {
-    width: 100,
-    height: 150,
-    marginBottom: 10,
+    width: 100,  // Establece el tamaño de la imagen a 100px
+    height: 150,  // Establece el tamaño de la imagen a 150px
+    marginBottom: 5,
+  },
+  bookTitle: {
+    textAlign: 'center',  // Centra el texto
+    fontSize: 16,
+    width: 100,  // Asegura que el texto tenga el mismo ancho que la imagen
+    marginTop: 5,  // Espacio entre la imagen y el título
   },
 });
-
