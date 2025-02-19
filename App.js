dac6781a23b557f7db543dd26fa10b1af63b5af6
Alex Gimenez <874113@unizar.es>
2025-02-19 12:47:53 +0100

    ModoOscuroPara3Pantallas

--- App.js
+++ App.js
@@ -3,12 +3,14 @@ import { StatusBar } from 'expo-status-bar';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { createDrawerNavigator } from "@react-navigation/drawer";
-import { Text } from 'react-native'; // Asegúrate de importar Text
+import { Text, View, StyleSheet } from 'react-native'; // Asegúrate de importar Text
 
 import Menu from './src/pantallas/Menu';
 import DetallesLibro from './src/componentes/DetallesLibro';
 import Foro from './src/pantallas/Foro';
 import LeerLibro from './src/pantallas/LeerLibro';
+
+import { useThemeColors } from './src/componentes/Tema';
 
 const Stack = createStackNavigator();
 const Drawer = createDrawerNavigator();
@@ -40,10 +42,25 @@ const MisListas = () => <Text>Mis Listas</Text>;
 // }
 
 export default function App() {
+
+  const colors = useThemeColors();
+
   return (
     <NavigationContainer>
-      <StatusBar style='auto' />
-      <Drawer.Navigator>
+      <StatusBar style={colors.text === '#ffffff' ? 'light' : 'dark'} />
+      <Drawer.Navigator
+        screenOptions={{
+          drawerStyle: {
+            backgroundColor: colors.background, // Fondo de la barra lateral
+          },
+          headerStyle: {
+            backgroundColor: colors.backgroundHeader, // Fondo del encabezado
+          },
+          headerTintColor: colors.text, // Color del texto del encabezado
+          drawerActiveTintColor: colors.text, // Color del texto activo en el drawer
+          drawerInactiveTintColor: colors.text === '#ffffff' ? '#d1d1d1' : '#000000', // Color del texto inactivo
+        }}
+      >
         <Drawer.Screen name="Inicio" component={MenuStack} options={{ headerShown: false }} />
         <Drawer.Screen name="Foro" component={Foro} options={{ headerShown: false }} />
         <Drawer.Screen name="Estadísticas" component={Estadisticas} options={{ headerShown: false }} />
@@ -59,6 +76,7 @@ export default function App() {
 
 // Función del stack para el menú principal y detalles
 function MenuStack() {
+  const colors = useThemeColors();
   return (
     <Stack.Navigator>
       <Stack.Screen 
@@ -69,12 +87,23 @@ function MenuStack() {
       <Stack.Screen 
         name="Detalles" 
         component={DetallesLibro} 
-        options={{ title: "Detalles del libro" }}
+        options={{
+          title: "Detalles del libro",
+          headerStyle: {
+            backgroundColor: colors.backgroundHeader, // Fondo oscuro o claro del encabezado
+          },
+          headerTintColor: colors.text, // Color del texto del título
+        }}
       />
       <Stack.Screen 
         name="LeerLibro" 
         component={LeerLibro} 
-        options={{ title: "Leyendo..." }}
+        options={{ title: "Leyendo...",
+          headerStyle: {
+            backgroundColor: colors.backgroundHeader, // Fondo oscuro o claro del encabezado
+          },
+          headerTintColor: colors.text, // Color del texto del título
+        }}
       />
     </Stack.Navigator>
   );
--- src/componentes/DetallesLibro.js
+++ src/componentes/DetallesLibro.js
@@ -4,10 +4,12 @@ import { faClock, faBook, faFileWord } from '@fortawesome/free-solid-svg-icons';
 
 import { StyleSheet, ScrollView, Text, View, TouchableOpacity } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
+import { useThemeColors } from "./Tema";
 
 export default function DetallesLibro({ route }) {
   const { libro } = route.params;
   const navigation = useNavigation();
+  const colors = useThemeColors();
 
   const handleAñadirALista = () => {
 
@@ -22,101 +24,100 @@ export default function DetallesLibro({ route }) {
   };
 
   return (
-    <ScrollView contentContainerStyle={stylesGeneral.container}>
+    <ScrollView contentContainerStyle={[stylesGeneral.container, { backgroundColor: colors.background }]}>
 
       {/* Botón de leer */}
       <TouchableOpacity 
-        style={stylesGeneral.boton} 
+        style={[stylesGeneral.boton, { backgroundColor: colors.button }]} 
         onPress={handleLeer}
       >
-        <Text style={stylesGeneral.textoBoton}>Leer</Text>
+        <Text style={[stylesGeneral.textoBoton, { color: colors.buttonText }]}>Leer</Text>
       </TouchableOpacity>
 
 
       {/* Título */}
       <View>
-        <Text style={stylesGeneral.titulo}>{libro.nombre}</Text>
-        <Text style={stylesGeneral.titulo}>de: {libro.autor}</Text>
+      <Text style={[stylesGeneral.titulo, { color: colors.text }]}>{libro.nombre}</Text>
+        <Text style={[stylesGeneral.titulo, { color: colors.text }]}>de: {libro.autor}</Text>
         <TouchableOpacity 
-          style={stylesGeneral.boton} 
+          style={[stylesGeneral.boton, { backgroundColor: colors.button }]} 
           onPress={handleAñadirALista}
         >
-          <Text style={stylesGeneral.textoBoton}>Añadir a lista</Text>
+          <Text style={[stylesGeneral.textoBoton, { color: colors.buttonText }]}>Añadir a lista</Text>
         </TouchableOpacity>
       </View>
 
-      <View style={stylesGeneral.linea}/>
+      <View style={[stylesGeneral.linea, { backgroundColor: colors.text }]} />
 
       {/* Más libros del autor */}
       {libro.autor !== "Anónimo" && (
         <View>
-          <Text style={stylesGeneral.titulo}>Más de {libro.autor}</Text>
+          <Text style={[stylesGeneral.titulo, { color: colors.text }]}>Más de {libro.autor}</Text>
             {/* Añadir más libros de ese autor */}
         </View>
       )}
 
-      <View style={stylesGeneral.linea}/>
+      <View style={[stylesGeneral.linea, { backgroundColor: colors.text }]} />
 
       {/* Sinopsis */}
       <View>
-        <Text style={stylesGeneral.titulo}>Sinopsis</Text>
-        <Text>{libro.resumen}</Text>
+        <Text style={[stylesGeneral.titulo, { color: colors.text }]}>Sinopsis</Text>
+        <Text style={{ color: colors.text }}>{libro.resumen}</Text>
       </View>
 
-      <View style={stylesGeneral.linea}/>
+      <View style={[stylesGeneral.linea, { backgroundColor: colors.text }]}/>
 
       {/* Acerca de este libro */}
       <View>
-        <Text style={stylesGeneral.titulo}>Acerca de este libro</Text>
+        <Text style={[stylesGeneral.titulo, { color: colors.text }]}>Acerca de este libro</Text>
         <View style={stylesAcercaDe.columnas3}>
           {/* Columna del número de páginas */}
           <View style={stylesAcercaDe.columna}>
-            <FontAwesomeIcon icon={faBook} style={stylesAcercaDe.icono} />
+            <FontAwesomeIcon icon={faBook} style={[stylesAcercaDe.icono, { color: colors.text }]} />
             <View style={stylesAcercaDe.textoSubcolumna}>
-              <Text>{libro.num_paginas}</Text>
-              <Text>páginas</Text>
+              <Text style={{ color: colors.text }}>{libro.num_paginas}</Text>
+              <Text style={{ color: colors.text }}>páginas</Text>
             </View>
           </View>
           {/* Columna del número de horas de lectura */}
           <View style={stylesAcercaDe.columna}>
-            <FontAwesomeIcon icon={faClock} style={stylesAcercaDe.icono}/>
+            <FontAwesomeIcon icon={faClock} style={[stylesAcercaDe.icono, { color: colors.text }]} />
             <View style={stylesAcercaDe.textoSubcolumna}>
-              <Text>{libro.horas_lectura}</Text>
-              <Text>horas de lectura</Text>
+              <Text style={{ color: colors.text }}>{libro.horas_lectura}</Text>
+              <Text style={{ color: colors.text }}>horas de lectura</Text>
             </View>
           </View>
           {/* Columna del número total de palabras */}
           <View style={stylesAcercaDe.columna}>
-            <FontAwesomeIcon icon={faFileWord} style={stylesAcercaDe.icono}/>
+          <FontAwesomeIcon icon={faFileWord} style={[stylesAcercaDe.icono, { color: colors.text }]} />
             <View style={stylesAcercaDe.textoSubcolumna}>
-              <Text>{libro.num_palabras}</Text>
-              <Text>total de palabras</Text>
+              <Text style={{ color: colors.text }}>{libro.num_palabras}</Text>
+              <Text style={{ color: colors.text }}>total de palabras</Text>
             </View>
           </View>
-
         </View>
       </View>
 
-      <View style={stylesGeneral.linea}/>
+      <View style={[stylesGeneral.linea, { backgroundColor: colors.text }]} />
 
       {/* Valoraciones del libro */}
       <View>
-        <Text style={stylesGeneral.titulo}>Valoraciones del libro:</Text>
+        <Text style={[stylesGeneral.titulo, { color: colors.text }]}>Valoraciones del libro:</Text>
         <TouchableOpacity 
-          style={stylesGeneral.boton} 
+          style={[stylesGeneral.boton, { backgroundColor: colors.button }]} 
           onPress={handleAñadirValoracion}
         >
-          <Text style={stylesGeneral.textoBoton}>Añadir valoración</Text>
+          <Text style={[stylesGeneral.textoBoton, { color: colors.buttonText }]}>Añadir valoración</Text>
         </TouchableOpacity>
       </View>
 
-      <View style={stylesGeneral.linea}/>
+      <View style={[stylesGeneral.linea, { backgroundColor: colors.text }]} />
 
       {/* Todas las reseñas del libro */}
       <View>
         <View>
-          <Text style={stylesGeneral.titulo}>Todas las reseñas del libro:</Text>
-          {/* Poner aquí el botón desplegable de ordenar por */}
+          <Text style={[stylesGeneral.titulo, { color: colors.text }]}>Todas las reseñas del libro:</Text>
+        {/* Poner aquí el botón desplegable de ordenar por */}
         </View>
         <View>
             {/* Poner aquí las reseñas */}
