41a466a4fd7733528d3e15d0db67b77ab446d804
ariana <874055@unizar.es>
2025-03-03 21:20:01 +0100

    Funciona el LECTOR, falta el zoom y ponerlo bonito

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 import { registerRootComponent } from 'expo';
 
+import 'react-native-gesture-handler';
+import 'react-native-reanimated';
+
 import App from './App';
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
